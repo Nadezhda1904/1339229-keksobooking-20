59d914e9aa7d79773582597dea593b31ea2db64c
Nadezhda Brazhnikova <b.nadejda.v@mail.ru>
2020-06-09 12:02:27 +0300

    Избавляет от цикла и вызывает функцию получения объекта

--- js/main.js
+++ js/main.js
@@ -14,43 +14,49 @@ var GUESTS_MIN = 1;
 var GUESTS_MAX = 3;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 var ArrayObjects = [];
 
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var generateArrayObjects = function (arr) {
-  for (var i = 0; i < COUNT_OF_OBJECTS; i++) {
-    arr.push({
-      'author': {
-        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-      },
-      'offer': {
-        'title': 'Заголовок объявления',
-        'address': getRandomNumber(0, map.offsetWidth) + ', ' + getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX),
-        'price': getRandomNumber(PRICE_MIN, PRICE_MAX),
-        'type': TYPES[getRandomNumber(0, TYPES.length - 1)],
-        'rooms': getRandomNumber(ROOMS_MIN, ROOMS_MAX),
-        'guests': getRandomNumber(GUESTS_MIN, GUESTS_MAX),
-        'checkin': CHECKIN[getRandomNumber(0, CHECKIN.length - 1)],
-        'checkout': CHECKOUT[getRandomNumber(0, CHECKOUT.length - 1)],
-        'features': FEATURES,
-        'description': 'Описание объявления',
-        'photos': PHOTOS[getRandomNumber(0, PHOTOS.length - 1)]
-      },
-      'location': {
-        'x': getRandomNumber(0, map.offsetWidth),
-        'y': getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX)
-      }
-    });
-  }
+var generateObjects = function (i) {
+  var Objects = {
+    'author': {
+      'avatar': getAvatarCount(i)
+    },
+
+    'offer': {
+      'title': 'Заголовок объявления',
+      'address': getRandomNumber(0, map.offsetWidth) + ', ' + getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX),
+      'price': getRandomNumber(PRICE_MIN, PRICE_MAX),
+      'type': TYPES[getRandomNumber(0, TYPES.length - 1)],
+      'rooms': getRandomNumber(ROOMS_MIN, ROOMS_MAX),
+      'guests': getRandomNumber(GUESTS_MIN, GUESTS_MAX),
+      'checkin': CHECKIN[getRandomNumber(0, CHECKIN.length - 1)],
+      'checkout': CHECKOUT[getRandomNumber(0, CHECKOUT.length - 1)],
+      'features': FEATURES,
+      'description': 'Описание объявления',
+      'photos': PHOTOS[getRandomNumber(0, PHOTOS.length - 1)]
+    },
+    'location': {
+      'x': getRandomNumber(0, map.offsetWidth),
+      'y': getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX)
+    }
+  };
+  return Objects;
 };
+
+function getAvatarCount(i) {
+  var avatarNumber = '0' + (i + 1);
+  return 'img/avatars/user' + avatarNumber + '.png';
+}
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
-generateArrayObjects(ArrayObjects);
 
 var pins = document.querySelector('.map__pins');
 var pinsTemplate = document.querySelector('#pin').content.querySelector('button');
@@ -59,8 +65,8 @@ var pinsFragment = document.createDocumentFragment();
 var generatePins = function (add) {
   var pin = pinsTemplate.cloneNode(true);
 
-  pin.style.left = add.location.x + 'px';
-  pin.style.top = add.location.y + 'px';
+  pin.style.left = add.location.x - PIN_WIDTH / 2 + 'px';
+  pin.style.top = add.location.y - PIN_HEIGHT + 'px';
 
   pin.querySelector('img').src = add.author.avatar;
   pin.querySelector('img').alt = add.offer.title;
@@ -68,7 +74,8 @@ var generatePins = function (add) {
   return pin;
 };
 
-for (var i = 0; i < ArrayObjects.length; i++) {
+for (var i = 0; i < COUNT_OF_OBJECTS; i++) {
+  ArrayObjects.push(generateObjects(i));
   pinsFragment.appendChild(generatePins(ArrayObjects[i]));
 }
 
